6ed892a66cd6f982f49a929e2378e7efc6878903
Dr. Ali Rahjouei <88965480+arahjou@users.noreply.github.com>
2022-10-19 08:42:45 +0200

    Update app.R

--- app.R
+++ app.R
@@ -1,85 +1,59 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+## app.R ##
 library(shiny)
+library(shinydashboard)
+library(ggplot2)
+library(ggrepel)
 
-# Define UI for application that draws a histogram
-ui <- fluidPage(
+#mac office
+#file = /Users/arahjou/Documents/Shinyapp/Rif1/
+mydata <- read.csv("Rif1_original.csv", sep = ",", header = TRUE)
+mydata <- mydata[c("id","Gene_Name","Protein_Descriptions","MW","PEP","Ratio", "Cut_off", "count", "PEP_log10")]
+#Filtering data
+mydata <- subset(mydata, mydata$count >= 4)
+mydata <- subset(mydata, mydata$PEP_log10 >= 1.3013)
+mydata <- subset(mydata, mydata$Ratio != 	"#DIV/0!")
+mydata <- subset(mydata, mydata$Cut_off != "xSD")
 
-    # Application title
-    titlePanel("Rif1 interactors (I-DIRT list)"),
 
-    # Sidebar with a slider input for number of bins 
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30),
-            radioButtons("Variablechoice", "Choice of Variable", 
-                         choices = c("Waiting Time", "Eruption Time"),
-                         selected = "Waiting Time"),
-            checkboxInput("axischoice", "Flip axisws?")
+
+#data gromming
+mydata$Ratio <- as.numeric(mydata$Ratio)
+mydata$Cut_off <- factor(mydata$Cut_off, levels=c("xSD", "1xSD", "2xSD", "3xSD", "4xSD"))
+
+if (interactive()) {
+    ui <- dashboardPage(
+        dashboardHeader(title = "Rif1 interactors"),
+        dashboardSidebar(
+            sidebarMenu(
+                textAreaInput("Rif1Interactors", "Please type one name per line \n and use gene symbol", value = "Rif1")
+            )
         ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-           plotOutput("distPlot"),
-           textOutput("usefullabel"),
-           verbatimTextOutput("summaryofvariable"),
-           plotOutput("scatterPlot")
+        dashboardBody(
+            # Boxes need to be put in a row (or column)
+            fluidRow(
+                box(plotOutput("plot1", height = 500, width = 650)),
+                tableOutput("information")
+            )
         )
     )
-)
-
-# Define server logic required to draw a histogram
-server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        if(input$Variablechoice == "Waiting Time") {x    <- faithful[, 2]}
-        if(input$Variablechoice == "Eruption Time") {x    <- faithful[, 1]}
+    
+    server <- function(input, output) {
         
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white', 
-             xlab=input$Variablechoice, 
-             main=paste("Histogram of" , input$Variablechoice))
-    })
-    
-    output$summaryofvariable <- renderPrint({
-        if(input$Variablechoice == "Waiting Time") {x    <- faithful[, 2]}
-        if(input$Variablechoice == "Eruption Time") {x    <- faithful[, 1]}
-        summary(x)
+        output$plot1 <- renderPlot({
+            data2 <- strsplit(input$Rif1Interactors, "\n")[[1]]
+            data1 <- mydata[ mydata$Gene_Name %in% c(data2), ]
+            #data1 <- mydata[ mydata$Gene_Name %in% c(input$Rif1Interactors), ]
+            data1$Ratio <- as.numeric(data1$Ratio)
+            ggplot(mydata, aes(x=Ratio, y=PEP_log10, color = Cut_off)) + 
+                geom_point(aes(size=log(count))) +  
+                geom_text_repel(data= data1, label = data1$Gene_Name, color="black", box.padding = 1, max.overlaps = Inf, size=6) +
+                labs(x = "H(H+L)", y ="Log(-Log(PEP))")
+        })
+        output$information <- renderTable(mydata[ mydata$Gene_Name %in% c(strsplit(input$Rif1Interactors, "\n")[[1]]), ])
+        #output$information <- renderTable(mydata[ mydata$Gene_Name %in% c(input$Rif1Interactors), ])
         
-    })
-    
-    
-    output$usefullabel <- renderText({
         
-        paste("Summary of" , input$Variablechoice, ":")
-    })
+    }
     
-    output$scatterPlot <- renderPlot({
-        if (input$axischoice == FALSE){
-        y <- faithful$eruptions
-        x <- faithful$waiting
-        plot(y~x, xlab="Wating Time", ylab = "Eruption Time")}
-        if (input$axischoice == TRUE){
-            x <- faithful$eruptions
-            y <- faithful$waiting
-            plot(y~x, xlab="Eruption Time", ylab = "Waiting Time")}
-    })
-    
+    shinyApp(ui, server)
 }
-
-# Run the application 
-shinyApp(ui = ui, server = server)
